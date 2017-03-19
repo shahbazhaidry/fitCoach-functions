14d41d6aab83e3b5d9ccc91571d9aa0aece12946
Jorge Vergara <javebratt@gmail.com>
2017-03-19 17:37:54 -0500

    Ready to ship

--- index.js
+++ index.js
@@ -44,75 +44,9 @@ exports.sendWeightUpdate = functions.database.ref('/userProfile/{userId}/weightT
             "sound":"default",
             "click_action":"FCM_PLUGIN_ACTIVITY"
           },
-          "data":{
-            "clientId": clientId
-          }
+          "data":{ "clientId": clientId }
         };
-
         return admin.messaging().sendToDevice(profileSnapshot.val().token, payload);
-
       });
-
-      
-      
     });
-
-
-/*
-    // Get inside the user's profile 
-
-  admin.database().ref(`/userProfile/${userId}/`).once('value', profileSnapshot => {
-    const coachTokenPromise = admin.database()
-      .ref(`/userProfile/${profileSnapshot.val().coachId}/notificationToken`).once('value');
-
-    const 
-    
-  });
-
-  const clientProfilePromise = admin.database
-  // Get the follower profile.
-  const getFollowerProfilePromise = admin.auth().getUser(followerUid);
-
-  return Promise.all([getDeviceTokensPromise, getFollowerProfilePromise]).then(results => {
-    const tokensSnapshot = results[0];
-    const follower = results[1];
-
-    // Check if there are any device tokens.
-    if (!tokensSnapshot.hasChildren()) {
-      return console.log('There are no notification tokens to send to.');
-    }
-    console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-    console.log('Fetched follower profile', follower);
-
-    // Notification details.
-    const payload = {
-      notification: {
-        title: 'You have a new follower!',
-        body: `${follower.displayName} is now following you.`,
-        icon: follower.photoURL
-      }
-    };
-
-    // Listing all tokens.
-    const tokens = Object.keys(tokensSnapshot.val());
-
-    // Send notifications to all tokens.
-    return admin.messaging().sendToDevice(tokens, payload).then(response => {
-      // For each message check if there was an error.
-      const tokensToRemove = [];
-      response.results.forEach((result, index) => {
-        const error = result.error;
-        if (error) {
-          console.error('Failure sending notification to', tokens[index], error);
-          // Cleanup the tokens who are not registered anymore.
-          if (error.code === 'messaging/invalid-registration-token' ||
-              error.code === 'messaging/registration-token-not-registered') {
-            tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-          }
-        }
-      });
-      return Promise.all(tokensToRemove);
-    });
-  });*/
-
 });
